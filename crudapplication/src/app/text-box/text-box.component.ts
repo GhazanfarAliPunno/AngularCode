71c7b8cd3a9343a9dff2f00742adc19c2c7f4827
punoo <ghazanfar.uetian@gmail.com>
2018-02-06 04:08:04 -0500

    updating components

--- crudapplication/src/app/text-box/text-box.component.ts
+++ crudapplication/src/app/text-box/text-box.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-text-box',
   templateUrl: './text-box.component.html',
   styleUrls: ['./text-box.component.css']
 })
-export class TextBoxComponent implements OnInit {
+export class TextBoxComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+ 
 
 }
