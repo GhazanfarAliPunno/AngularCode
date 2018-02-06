2f56d632078b95afcc583ae71d92840886454edf
punoo <ghazanfar.uetian@gmail.com>
2018-02-06 04:06:43 -0500

    updating component

--- crudapplication/src/app/buttons/buttons.component.ts
+++ crudapplication/src/app/buttons/buttons.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-buttons',
   templateUrl: './buttons.component.html',
   styleUrls: ['./buttons.component.css']
 })
-export class ButtonsComponent implements OnInit {
+export class ButtonsComponent  {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  
 }
